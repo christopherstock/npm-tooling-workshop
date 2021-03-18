759c93c613f75330dde9d5b8a194e2b7a5454056
Christopher Stock <christopher.stock@mayflower.de>
2021-03-18 11:32:28 +0100

    Complete babylonjs example.

--- src/index.ts
+++ src/index.ts
@@ -18,34 +18,37 @@ window.onload = () => {
     // create BABYLON scene
     const scene = new BABYLON.Scene(engine);
     scene.clearColor = new BABYLON.Color4(0.0, 0.0, 0.0, 1.0);
-    const camera = new BABYLON.ArcRotateCamera("camera", -Math.PI / 2, Math.PI / 2.5, 3, new BABYLON.Vector3(0, 0, 0), scene);
+    const camera = new BABYLON.ArcRotateCamera("camera", -Math.PI / 0.77, Math.PI / 2.5, 3, new BABYLON.Vector3(0, 0, 0), scene);
     camera.attachControl(canvas, true);
-    const light = new BABYLON.PointLight("light", new BABYLON.Vector3(0, 0, 2), scene);
-    light.intensity = 2.5;
-    // light.diffuse   = new BABYLON.Color3( 1.0, 1.0, 1.0 );
-    // light.specular  = new BABYLON.Color3( 1.0, 1.0, 1.0 );
-    light.range     = 3.0;
-
+    const hemisphericLight = new BABYLON.HemisphericLight("light", new BABYLON.Vector3(1, 0, 0), scene);
+    hemisphericLight.intensity = 0.1;
+    const pointLightA = new BABYLON.PointLight("light", new BABYLON.Vector3(0, 0, 2), scene);
+    pointLightA.intensity = 2.0;
+    pointLightA.range     = 2.0;
+    pointLightA.diffuse   = new BABYLON.Color3( 1.0, 1.0, 1.0 );
+    pointLightA.specular  = new BABYLON.Color3( 1.0, 1.0, 1.0 );
+    const pointLightB = new BABYLON.PointLight("light", new BABYLON.Vector3(0, 0, -2), scene);
+    pointLightB.intensity = 2.0;
+    pointLightB.range     = 2.0;
+    pointLightB.diffuse   = new BABYLON.Color3( 1.0, 0.0, 1.0 );
+    pointLightB.specular  = new BABYLON.Color3( 1.0, 1.0, 1.0 );
     const materialBox = new BABYLON.StandardMaterial('material', scene );
     materialBox.emissiveColor = new BABYLON.Color3( 0.89, 0.32, 0.01 );
-    materialBox.specularColor = new BABYLON.Color3( 0.0, 1.0, 0.0 );
+    materialBox.specularColor = new BABYLON.Color3( 1.0, 1.0, 1.0 );
     const box = BABYLON.MeshBuilder.CreateBox("box", {});
     box.material = materialBox;
-
     const materialSphere = new BABYLON.StandardMaterial('material', scene );
-    materialSphere.emissiveColor = new BABYLON.Color3( 0.89, 0.32, 0.01 );
-    const sphere = BABYLON.MeshBuilder.CreateSphere("box", {diameter: 0.25,});
-    sphere.position = new BABYLON.Vector3( 0.0, 0.0, 1.0 );
-    sphere.position = new BABYLON.Vector3( 0.0, 0.0, 1.0 );
+    materialSphere.emissiveColor = new BABYLON.Color3( 0.53, 0.53, 0.53 );
+    const sphereA = BABYLON.MeshBuilder.CreateSphere("box", {diameter: 0.4,});
+    sphereA.material = materialSphere;
+    sphereA.position = new BABYLON.Vector3( 0.0, 0.0, 1.0 );
+    const sphereB = BABYLON.MeshBuilder.CreateSphere("box", {diameter: 0.4,});
+    sphereB.material = materialSphere;
+    sphereB.position = new BABYLON.Vector3( 0.0, 0.0, -1.0 );
 
     // register render loop that repeatedly renders the scene
     engine.runRenderLoop(() => {
-        camera.cameraRotation.add(new BABYLON.Vector2(1.0, 1.0));
-
-        // box.rotate(BABYLON.Vector3.Up(), 0.01);
-        // box.rotate(BABYLON.Vector3.Right(), 0.005);
-        // box.rotate(BABYLON.Vector3.Backward(), 0.001);
-
+        camera.alpha += 0.025;
         scene.render();
     });
 }
