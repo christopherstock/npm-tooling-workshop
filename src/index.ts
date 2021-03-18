ad62b27fb20dd586d58bfa03979e64f2b3ed18e2
Christopher Stock <christopher.stock@mayflower.de>
2021-03-18 10:58:00 +0100

    Revise babylonjs example.

--- src/index.ts
+++ src/index.ts
@@ -20,18 +20,31 @@ window.onload = () => {
     scene.clearColor = new BABYLON.Color4(0.0, 0.0, 0.0, 1.0);
     const camera = new BABYLON.ArcRotateCamera("camera", -Math.PI / 2, Math.PI / 2.5, 3, new BABYLON.Vector3(0, 0, 0), scene);
     camera.attachControl(canvas, true);
+    const light = new BABYLON.PointLight("light", new BABYLON.Vector3(0, 0, 2), scene);
+    light.intensity = 2.5;
+    // light.diffuse   = new BABYLON.Color3( 1.0, 1.0, 1.0 );
+    // light.specular  = new BABYLON.Color3( 1.0, 1.0, 1.0 );
+    light.range     = 3.0;
 
-//    const light = new BABYLON.HemisphericLight("light", new BABYLON.Vector3(0, 1, 0), scene);
-    const light = new BABYLON.HemisphericLight("light", new BABYLON.Vector3(1, 0, 0), scene);
-
-    light.groundColor = new BABYLON.Color3( 0.89, 0.32, 0.01 );
+    const materialBox = new BABYLON.StandardMaterial('material', scene );
+    materialBox.emissiveColor = new BABYLON.Color3( 0.89, 0.32, 0.01 );
+    materialBox.specularColor = new BABYLON.Color3( 0.0, 1.0, 0.0 );
     const box = BABYLON.MeshBuilder.CreateBox("box", {});
+    box.material = materialBox;
+
+    const materialSphere = new BABYLON.StandardMaterial('material', scene );
+    materialSphere.emissiveColor = new BABYLON.Color3( 0.89, 0.32, 0.01 );
+    const sphere = BABYLON.MeshBuilder.CreateSphere("box", {diameter: 0.25,});
+    sphere.position = new BABYLON.Vector3( 0.0, 0.0, 1.0 );
+    sphere.position = new BABYLON.Vector3( 0.0, 0.0, 1.0 );
 
     // register render loop that repeatedly renders the scene
     engine.runRenderLoop(() => {
-        box.rotate(BABYLON.Vector3.Up(), 0.01);
-        box.rotate(BABYLON.Vector3.Right(), 0.005);
-        box.rotate(BABYLON.Vector3.Backward(), 0.001);
+        camera.cameraRotation.add(new BABYLON.Vector2(1.0, 1.0));
+
+        // box.rotate(BABYLON.Vector3.Up(), 0.01);
+        // box.rotate(BABYLON.Vector3.Right(), 0.005);
+        // box.rotate(BABYLON.Vector3.Backward(), 0.001);
 
         scene.render();
     });
